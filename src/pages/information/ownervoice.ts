f782c2bb8fb1c002ce3196bfbe6bec1ddcb3fc8d
lichaolin <lichaolin@m-apps.com.cn>
2021-11-08 12:03:02 +0800

    提交修改

--- src/pages/information/ownervoice.ts
+++ src/pages/information/ownervoice.ts
@@ -1,6 +1,26 @@
-import { Vue, Component } from 'vue-property-decorator';
-
+import { Vue, Component, Watch } from 'vue-property-decorator';
+import utils from 'root/utils';
+import { Events } from 'root/utils/EnumUtils';
 @Component
 export default class OwnerVoice extends Vue {
-
+	///banner1
+	ownerBannerOptions: any = {
+		centeredSlides: true,
+		speed: 500,
+		spaceBetween: 30,
+		slidesPerView: 2,
+		loop: true,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false
+		},
+		pagination: {
+			// el: '.pagination2',
+			// clickable: true
+		},
+		navigation: {
+			// nextEl: '.next2',
+			// prevEl: '.prev2'
+		}
+	};
 }
--- src/pages/information/ownervoice.vue
+++ src/pages/information/ownervoice.vue
@@ -1,6 +1,50 @@
 <template>
-	<div class="owner-voice">
+	<div class="owner">
 		<!-- 业主心声 -->
+		<div class="owner-vioce">
+			<h2>业主心声</h2>
+			<h3>OWEWE</h3>
+			<div class="swiper-box">
+				<swiper :options="ownerBannerOptions" ref="ownerBanner">
+					<swiper-slide v-for="n in 3" :key="n">
+						<div class="img-box">
+							<img />
+							<div class="left-info">
+								<div class="inner-box">
+									asdas
+								</div>
+							</div>
+						</div>
+					</swiper-slide>
+				</swiper>
+			</div>
+		</div>
+		<div class="dian-ping">
+			<h2>大众点评</h2>
+			<h3>sadas</h3>
+			<div class="swiper-box">
+				swiper
+				<swiper>
+					<swiper-slide>1</swiper-slide>
+					<swiper-slide>2</swiper-slide>
+					<swiper-slide>3</swiper-slide>
+				</swiper>
+			</div>
+		</div>
+		<div class="flag-honer">
+			<h2>荣誉锦旗</h2>
+			<h3>sadwqe</h3>
+			<div class="swiper-box">
+				<swiper>
+					<swiper-slide>1</swiper-slide>
+					<swiper-slide>2</swiper-slide>
+					<swiper-slide>3</swiper-slide>
+					<swiper-slide>4</swiper-slide>
+					<swiper-slide>5</swiper-slide>
+					<swiper-slide>6</swiper-slide>
+				</swiper>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -10,5 +54,87 @@ export default OwnerVoice;
 </script>
 
 <style lang="scss">
-
+.owner {
+	font-size: 16px;
+	img {
+		vertical-align: bottom;
+	}
+	@mixin title {
+		h2,
+		h3 {
+			font-weight: 700;
+			font-size: 36px;
+			text-align: center;
+		}
+		h2 {
+			margin-top: 140px;
+		}
+		h3 {
+			margin-bottom: 50px;
+		}
+	}
+	.owner-vioce {
+		@include title();
+		.swiper-box {
+			height: 500px;
+			.img-box {
+				position: relative;
+				height: 500px;
+				opacity: 0.5;
+				transition: all 0.3s;
+				img {
+					width: 100%;
+					height: 100%;
+					background-color: skyblue;
+				}
+				.left-info {
+					overflow: hidden;
+					position: absolute;
+					top: 50%;
+					transform: translateY(-50%);
+					left: -200px;
+					width: 300px;
+					height: 400px;
+					.inner-box {
+						position: absolute;
+						width: 100%;
+						height: 100%;
+						background: #000;
+						left: 100%;
+					}
+				}
+			}
+			/deep/.swiper {
+				display: none;
+			}
+			.swiper-slide-active {
+				.img-box {
+					opacity: 1;
+					transition: all .3s;
+					.left-info {
+						width: 300px;
+						.inner-box {
+							transition: all 1s .5s;
+							left: 0px;
+						}
+					}
+				}
+			}
+		}
+	}
+	.dian-ping {
+		background-color: aqua;
+		@include title();
+		.swiper-box {
+			height: 500px;
+		}
+	}
+	.flag-honer {
+		padding: 30px;
+		@include title();
+		.swiper-box {
+			height: 500px;
+		}
+	}
+}
 </style>
