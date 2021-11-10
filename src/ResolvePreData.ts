a82b9b161013594d5cd18424c38f7abbd23c2774
wenxi <wenxi@m-apps.com.cn>
2021-11-10 11:51:48 +0800

    请求注释

--- src/ResolvePreData.ts
+++ src/ResolvePreData.ts
@@ -4,27 +4,29 @@ import utils from "./utils";
 export default class ResolvePreData {
 	static getFooterData() {
 		return new Promise((resolve: (value?: unknown) => void, reject: (reason?: any) => void) => {
-      if (store.state.footData) {
-        return resolve();
-      }
-      utils.service.queryFooterData(res => {
-        let data = res.data;
-        store.state.footData = data;
-        resolve();
-      });
+      // if (store.state.footData) {
+      //   return resolve();
+      // }
+      // utils.service.queryFooterData(res => {
+      //   let data = res.data;
+      //   store.state.footData = data;
+      //   resolve();
+      // });
+			resolve();
     })
 	}
 
 	static getMenuData() {
 		return new Promise((resolve: (value?: unknown) => void, reject: (reason?: any) => void) => {
-      if (store.state.menuData) {
-        return resolve();
-      }
-      utils.service.queryMenuData(res => {
-        let data = res.data;
-        store.state.menuData = data;
-        resolve();
-      });
+      // if (store.state.menuData) {
+      //   return resolve();
+      // }
+      // utils.service.queryMenuData(res => {
+      //   let data = res.data;
+      //   store.state.menuData = data;
+      //   resolve();
+      // });
+			resolve();
     })
 	}
 }
