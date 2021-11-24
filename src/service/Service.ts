8356e6d522dc44aefe231de0b319cc89786fd633
wenxi <wenxi@m-apps.com.cn>
2021-11-24 10:04:08 +0800

    service数据处理

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -6,29 +6,29 @@ axios.defaults.headers['content-type'] = 'application/x-www-form-urlencoded';
 axios.defaults.timeout = 60000;
 
 export class Service extends EventEmitter {
-    protected send(url: string, data: any, success: ISuccess, error?: IError) {
-        axios.post(url, qs.stringify(data)).then((res: AxiosResponse) => {
-            console.log(res);
-            return success(res);
-        }, (err) => {
-						if (err) {
-							console.error("请求出错!");
-						}
-            return error && error(0);
-        });
-    }
+	protected send(url: string, data: any, success: ISuccess, error?: IError) {
+		axios.post(url, qs.stringify(data)).then((res: AxiosResponse) => {
+			res.data = JSON.parse(res.data);
+			return success(res);
+		}, (err) => {
+			if (err) {
+				console.error("请求出错!");
+			}
+			return error && error(0);
+		});
+	}
 
-		public queryFooterData(callback: (res: any) => void) {
-			this.send(`/api.php/datahub/foot`, {}, callback);
-		}
+	public queryFooterData(callback: (res: any) => void) {
+		this.send(`/api.php/datahub/foot`, {}, callback);
+	}
 
-		public queryMenuData(callback: (res: any) => void) {
-			this.send('/api.php/datahub/menu', {}, callback);
-		}
+	public queryMenuData(callback: (res: any) => void) {
+		this.send('/api.php/datahub/menu', {}, callback);
+	}
 
-		///精选案例列表
-		public queryJxCase(req, callback: (res: any) => void) {
-			this.send('api.php/Ajax/jxCase', req, callback);
-		}
+	///精选案例列表
+	public queryJxCase(req, callback: (res: any) => void) {
+		this.send('api.php/Ajax/jxCase', req, callback);
+	}
 
 }
