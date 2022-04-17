00eb4e8c47a7c93ece754f7cd9d64175b4b43584
wenxi <1129130574@qq.com>
2022-04-17 22:54:22 +0800

    代理配置修改

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,37 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = (req, res) => {
+// 	let target = "";
+
+// 	if (req.url.startsWith('/api.php')) {
+// 		target = "http://btzs.shbotao.net/";
+// 	}
+
+// 	createProxyMiddleware({
+// 		target,
+// 		changeOrigin: true
+// 	})(req, res);
+// }
+
+
+const request = require('request');
 
 module.exports = (req, res) => {
-	let target = "";
+  // proxy middleware options
+  let prefix = "/api.php"
+  if (!req.url.startsWith(prefix)) {
+    return;
+  }
+  let target = "http://btzs.shbotao.net" + req.url;
 
-	if (req.url.startsWith('/api.php')) {
-		target = "http://btzs.shbotao.net/";
-	}
-
-	createProxyMiddleware({
-		target,
-		changeOrigin: true
-	})(req, res);
+  let options = {
+    'method': 'POST',
+    'url': target
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(response.body);
+    res.end();
+  });
 }
